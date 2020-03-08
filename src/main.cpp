9024b7e5615729b11d4773946c8a0a41a386346c
Temur Tchanukvadze <t.chanukvadze@gmail.com>
2020-03-08 18:29:10 +0400

    add bmp sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
 #include <DHT.h>
+#include <SFE_BMP180.h>
 
 int readMq135(uint8_t pin);
 
 void handle_serial(int bytes);
 
 enum Command {
-    MQ135 = 65,
-    Dht = 66,
+    MQ135 = 'A',
+    Dht = 'B',
+    Bmp180 = 'C',
 };
 
 
@@ -38,11 +40,91 @@ public:
     }
 };
 
+// 0 to 3, higher numbers are slower, higher-res outputs.
+const char BMP_OVERSAMPLING = 3;
+
+class BmpDriver {
+    double pressure = 0;
+    double temp = 0;
+    SFE_BMP180 bmp;
+public:
+    BmpDriver(SFE_BMP180 bmp) : bmp(bmp) {
+    }
+
+    void begin() {
+        this->bmp.begin();
+    }
+
+    /**
+     *
+     * @returns true if measurement succeed
+     */
+    boolean measurePressure() {
+        char status;
+        double T, P;
+
+        // will return delay in ms to wait, or 0 if I2C error
+        status = this->bmp.startTemperature();
+        if (status == 0) return false;
+
+        // wait for temp measurement
+        delay(status);
+
+        status = this->bmp.getTemperature(T);
+        if (status == 0) return false;
+
+        // will return delay in ms to wait, or 0 if I2C error.
+        status = this->bmp.startPressure(BMP_OVERSAMPLING);
+        if (status == 0) return false;
+
+        // wait for pressure measurement
+        delay(status);
+
+        status = this->bmp.getPressure(P, T);
+        if (status == 0) return false;
+
+        //
+        this->pressure = P;
+        this->temp = T;
+        return true;
+    }
+
+    /**
+     * @description
+     * should be called after measurePressure()
+     * @returns pressure
+     */
+    double getPressure() {
+        return this->pressure;
+    }
+
+    /**
+     * @description
+     * should be called after measurePressure()
+     * @returns pressure
+     */
+    double getTemperature() {
+        return this->temp;
+    }
+
+    // 0 = Success
+    // 1 = Data too long to fit in transmit buffer
+    // 2 = Received NACK on transmit of address
+    // 3 = Received NACK on transmit of data
+    // 4 = Other error
+    char getError() {
+        this->bmp.getError();
+    }
+};
+
+
 auto dhtDriver = new DhtDriver(DHT(DHT_PIN, DHT22));
+auto bmpDriver = new BmpDriver(SFE_BMP180());
 
 void setup() {
     Serial.begin(9600);
     dhtDriver->begin();
+    bmpDriver->begin();
 }
 
 
@@ -58,14 +140,24 @@ void handle_serial(int byte) {
 
     switch (command) {
         case MQ135: {
-            int air_quality = readMq135(MQ135_PIN);
+            const int air_quality = readMq135(MQ135_PIN);
             send(command, (String) air_quality);
             break;
         }
         case Dht: {
-            float temp = dhtDriver->readTemperature();
-            float humidity = dhtDriver->readHumidity();
+            const float temp = dhtDriver->readTemperature();
+            const float humidity = dhtDriver->readHumidity();
             send(command, (String) temp + DATA_SEPARATOR + (String) humidity);
+            break;
+        }
+        case Bmp180: {
+            if (bmpDriver->measurePressure()) {
+                const double pressure = bmpDriver->getPressure();
+                const double temp = bmpDriver->getTemperature();
+                send(command, (String) pressure + DATA_SEPARATOR + (String) temp);
+            } else {
+                Serial.println(bmpDriver->getError());
+            }
             break;
         }
     }
@@ -73,7 +165,7 @@ void handle_serial(int byte) {
 
 
 void send(Command command, String data) {
-    // example: ^65&45$
+    // example 1: ^65&45$
     String response = RESPONSE_PREFIX + command + COMMAND_SUFFIX + data + RESPONSE_SUFFIX;
     Serial.println(response);
 }
