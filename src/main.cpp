062c6da6766dc16a54b174ae0c9b2e64090f1043
Temur Tchanukvadze <t.chanukvadze@gmail.com>
2020-03-08 16:34:44 +0400

    add dht sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,48 @@
 #include <Arduino.h>
+#include <DHT.h>
 
-int read_mq135(uint8_t pin);
+int readMq135(uint8_t pin);
+
 void handle_serial(int bytes);
+
 enum Command {
     MQ135 = 65,
+    Dht = 66,
 };
+
+
 void send(Command command, String data);
 
-const int MQ135_PIN = 0; // A0
+const String RESPONSE_PREFIX = "^";
+const String COMMAND_SUFFIX = "&";
+const String RESPONSE_SUFFIX = "$";
+const String DATA_SEPARATOR = "|";
+const uint8_t MQ135_PIN = 0; // A0
+const uint8_t DHT_PIN = 2; // D2
+class DhtDriver {
+    DHT dht;
+public:
+    DhtDriver(DHT dht) : dht(dht) {
+    }
 
+    void begin() {
+        this->dht.begin();
+    }
+
+    float readHumidity() {
+        return this->dht.readHumidity();
+    }
+
+    float readTemperature() {
+        return this->dht.readTemperature();
+    }
+};
+
+auto dhtDriver = new DhtDriver(DHT(DHT_PIN, DHT22));
 
 void setup() {
     Serial.begin(9600);
+    dhtDriver->begin();
 }
 
 
@@ -23,22 +54,23 @@ void loop() {
 
 
 void handle_serial(int byte) {
-    const auto command = (Command)byte;
+    const auto command = (Command) byte;
 
     switch (command) {
-        case MQ135:
-            int air_quality = read_mq135(MQ135_PIN);
-            send(command, (String)air_quality);
+        case MQ135: {
+            int air_quality = readMq135(MQ135_PIN);
+            send(command, (String) air_quality);
             break;
+        }
+        case Dht: {
+            float temp = dhtDriver->readTemperature();
+            float humidity = dhtDriver->readHumidity();
+            send(command, (String) temp + DATA_SEPARATOR + (String) humidity);
+            break;
+        }
     }
-
-    // sensorValue = read_mq135(MQ135_PIN);
-    // Serial.println(sensorValue, DEC); // prints the value read
 }
 
-const String RESPONSE_PREFIX = "^";
-const String COMMAND_SUFFIX = "&";
-const String RESPONSE_SUFFIX = "$";
 
 void send(Command command, String data) {
     // example: ^65&45$
@@ -47,8 +79,7 @@ void send(Command command, String data) {
 }
 
 
-
 // commands
-int read_mq135(uint8_t pin) {
+int readMq135(uint8_t pin) {
     return analogRead(pin);
 }
